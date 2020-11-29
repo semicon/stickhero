be95f1cfe8b14646c5ec9bbd063081d523d90c74
guruchian <30399464+semicon@users.noreply.github.com>
2020-11-29 11:32:28 +0700

    herostick

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1606624206,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/hero-sheet0.png",
+		"images/player-sheet0.png",
+		"images/bar-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/ground.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
