ccdde038c7e7e3b0c1ab18cac7c1a63d63700032
guruchian <30399464+semicon@users.noreply.github.com>
2020-11-29 13:18:56 +0700

    strickhero

--- offline.js
+++ offline.js
@@ -1,11 +1,13 @@
 ﻿{
-	"version": 1606629435,
+	"version": 1606630370,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/hero-sheet0.png",
+		"images/hero-sheet1.png",
+		"images/hero-sheet2.png",
 		"images/player-sheet0.png",
 		"images/bar-sheet0.png",
 		"images/sprite-sheet0.png",
